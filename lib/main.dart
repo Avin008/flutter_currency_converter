af51ad67e3efbc36ec34cb540106fefca7ffcc17
Avinash <avinashnanda008@gmail.com>
2023-07-27 20:30:34 +0530

    completed screens

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: const Text("Currency Converter"),
+        centerTitle: true,
+      ),
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
@@ -21,26 +27,38 @@ class MyApp extends StatelessWidget {
               style: TextStyle(
                   fontSize: 40,
                   fontWeight: FontWeight.bold,
-                  color: Colors.white),
+                  color: Colors.white60),
             ),
-            const TextField(
-              decoration: InputDecoration(
-                  prefixIcon:
-                      Icon(Icons.monetization_on_outlined, color: Colors.black),
-                  hintText: "Enter Amount in INR",
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 20, color: Colors.black))),
-            ),
-            ElevatedButton(
-              onPressed: () => {},
-              style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.black)),
-              child: const Text(
-                "Convert",
-                style: TextStyle(color: Colors.white),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: const TextField(
+                keyboardType: TextInputType.numberWithOptions(
+                    decimal: true, signed: true),
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    prefixIcon: Icon(Icons.monetization_on_outlined,
+                        color: Colors.black54),
+                    hintText: "Please enter the amount in USD",
+                    filled: true,
+                    fillColor: Colors.white60,
+                    iconColor: Colors.black54),
               ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      minimumSize: const Size(double.infinity, 45),
+                      backgroundColor: Colors.black,
+                      shadowColor: Colors.black,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(5)))),
+                  onPressed: () => {},
+                  child: const Text("Convert")),
             )
           ],
         ),
